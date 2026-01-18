7baf3788d08a00e06e1210cf9e1b3fc164244909
wpzy.cc <138046438+wpzycc@users.noreply.github.com>
2026-01-18 10:19:18 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // js/script.js - 终极修复版 v4 (修复下载按钮闪烁问题)
 
 // 默认示例 API。
-const API_URL = 'https://dh.lt6.ltd/xjj/video.php';
+const API_URL = 'https://pic.ltywl.top/mn/api.php';
 
 // ----- IndexedDB 简单封装 -----
 const DB_NAME = 'random_beauty_db';
@@ -959,6 +959,7 @@ window.clearHistory = clearHistory;
 window.clearFavorites = clearFavorites;
 window.clearCache = clearCache;
 window.clearStats = clearStats;
+
 
 
 
